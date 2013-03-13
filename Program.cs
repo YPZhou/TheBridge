65e5ed79f7fec3e39d5610de2b66519595c78224
Yunpeng Zhou <argoran@gmail.com>
2013-03-13 14:46:05 +0100

    initial import

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SFML.Window;
+using SFML.Graphics;
+
+namespace TheBridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(800, 600), "The Bridge", Styles.Fullscreen);
+
+            while (window.IsOpen())
+            {
+                window.DispatchEvents();
+
+                if (Keyboard.IsKeyPressed(Keyboard.Key.Escape))
+                {
+                    window.Close();
+                }
+
+                window.Clear(Color.Black);
+                //Update();
+                //Render(window);
+                window.Display();
+            }
+        }
+    }
+}
